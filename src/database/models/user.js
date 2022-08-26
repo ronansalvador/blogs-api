32e40dbd4ab75d22ad93a3885b2f24f869d3c1ad
Ronan Salvador <ronansalvador@yahoo.com.br>
2022-08-26 13:55:31 -0300

    User Model - req02

---
+++ src/database/models/user.js
@@ -0,0 +1,26 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  },{
+    tableName: 'Users',
+    timestamps: false,
+  });
+
+  User.associate = (models) => {
+    User.hasMany(models.BlogPost, {
+      foreignKey: 'userId',
+      as: 'blogpost',
+    });
+  };
+
+  return User;
+}; 
